8baf0abaf8c43d3a0a22b936dfae619ea6099f08
vinaybadola <vinaybadola.cs18@gmail.com>
2024-06-11 18:07:52 +0530

    added the vue cli basics

---
+++ src/components/DemoSlot.vue
@@ -0,0 +1,25 @@
+<template>
+    <slot>Default Content</slot>    <!--The "default content" is the fallback slot when the parent component doesn't have any slot present. 
+                                    To test this to remove the content written inside <DemoSlot>   </DemoSlot> in parent component    -->
+    <div class="actions">
+        <slot name="links"></slot>
+    </div>
+</template>
+
+<style scoped>
+
+    .actions{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: #f1f1f1;
+        padding: 10px;
+    }
+    .actions a{
+        text-decoration: none;
+        color: #000;
+    }
+    .actions a:hover{
+        color: #03cfb4;
+    }
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <!--
+
+        The class sale is the example of how we can pass  the props to the child component as a class and
+        if the value is sale it will apply the css to the modal otherwise it will not apply the css. Props are optional 
+        
+        -->
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <h1>Modal Title</h1>
+            <p>Modal Content</p>
+            <h3>{{ msg }}</h3>
+            <p>{{ title }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['msg', 'title', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        border-radius: 10px;
+        background: #fff;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    /*  
+        Either we can use .modal p to include the css for p tag in this component only or we can use scoped to limit the css to this component only in the style tag above 
+    */
+    .modal p{
+        font-style: normal;
+    }
+    .modal.sale{
+        background: #f00;
+        color: white;
+    }
+    .modal.sale h1{
+        color: #fff;
+    }
+
+</style>
