35ac026d2b1ef791faffbdbffbe1d283c7c32b3c
vinaybadola <vinaybadola.cs18@gmail.com>
2024-06-25 02:25:42 +0530

    understand working of teleport

---
+++ src/components/TelePortComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1> {{ buttonText }}</h1>
+    <p>This component is the example of how we mount a component into '#modals'  div  using teleport</p>
+</template>
+
+<script>
+export default {
+    name: 'TelePortComponent',
+    props: {
+        buttonText: {
+            type: String,
+            default: '',
+        },
+    },
+};
+</script>
